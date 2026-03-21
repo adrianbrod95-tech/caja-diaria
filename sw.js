a1b12da1df34d763beee405a41f1b30466756372
adrianbrod95-tech <adrianbrod95@gmail.com>
2026-03-21 09:57:39 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'caja-v4';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        '/caja-diaria/',
+        '/caja-diaria/index.html',
+        '/caja-diaria/manifest.json'
+      ]).catch(() => {})
+    )
+  );
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (!e.request.url.startsWith('https://adrianbrod95-tech.github.io')) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === 'basic') {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        }
+        return res;
+      }).catch(() => caches.match('/caja-diaria/index.html'));
+    })
+  );
+});
